dc257349dca384f632a02018daa8879ef830e08d
Neerjaka <suman4uneeraj@gmail.com>
2025-08-31 21:49:20 +0530

    Update main.tf

--- S3/s3_backend/main.tf
+++ S3/s3_backend/main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
-  region = "us-west-1"
+ region = "us-east-1"
 }
-terraform {
-  backend "s3"{
-      bucket =  "my-tfstate-bucket-wsa"
-      key =  "prod/ec2/terraform.tfstate"
-      region =  "us-west-1"
-      dynamodb_table = "my-terraform-locks"
-      encrypt = true
-    }
-  }
