ca6178799327ec4ea5436c23ef8906a27a22ae20
Neerjaka <suman4uneeraj@gmail.com>
2025-08-31 22:05:19 +0530

    Create output.tf

---
+++ EC2/output.tf
@@ -0,0 +1,15 @@
+output "instance_id" {
+value = aws_instance.my_lab_4.id
+}
+
+output "Instance_public_ip" {
+value = aws_instance.my_lab_4.public_ip
+}
+
+output "Instance_Public_DNS" {
+value = aws_instance.my_lab_4.public_dns
+}
+
+output "Instanc_ARN" {
+value = aws_instance.my_lab_4.arn
+}
